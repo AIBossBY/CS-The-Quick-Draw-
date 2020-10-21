bd84fb72d46926618071e1c168237ec70dbf12e5
YohannPerron <yohann.perron@gmail.com>
2020-10-21 19:24:14 +0200

    solve problems

--- learning.py
+++ learning.py
@@ -8,17 +8,17 @@ n=len(dataset)
 
 #shuffle and divide
 dataset.shuffle(n)
-train_dataset = dataset.take(int(n*0.8)) 
-test_dataset = dataset.skip(int(n*0.8))
+train_dataset = dataset.take(int(n*0.8)).batch(1000)
+test_dataset = dataset.skip(int(n*0.8)).batch(1000)
 
-#train_dataset.prefetch(n)
+train_dataset.prefetch(2)
 
 #model
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
+  tf.keras.layers.Dense(100)
 ])
 
 #loss
@@ -29,4 +29,4 @@ model.compile(optimizer='Adam', loss=loss_fn, metrics=['accuracy'])
 model.fit(train_dataset, epochs=50)
 
 #evaluate
-model.evaluate(x_test,  y_test, verbose=2)
+model.evaluate(test_dataset, verbose=2)
